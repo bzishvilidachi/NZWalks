b5cd7557b4c93db60797d8fd054a20c77650c616
Dachi Bzishvili <bzishvilidachi1@gmail.com>
2024-11-18 17:43:56 +0400

    Add project files.

---
+++ NZWalksAPI/Controllers/RegionsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NZWalksAPI.Data;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RegionsController : ControllerBase
+	{
+		private readonly NZWalksDbContext _context;
+		public RegionsController(NZWalksDbContext context)
+		{
+			context = _context;
+		}
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			var regions = _context.Regions.ToList();
+			return Ok(regions);
+		}
+
+
+		[HttpGet]
+		[Route("{id}")]
+		public IActionResult GetById(Guid id)
+		{
+			var region = _context.Regions.FirstOrDefault(u => u.Id == id);
+
+			if (region == null)
+			{
+				return NotFound();
+			}
+			else
+			{
+				return Ok(region);
+			}
+		}
+	}
+}
